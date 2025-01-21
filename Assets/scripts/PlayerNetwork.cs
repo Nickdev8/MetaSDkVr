f58782a7a0f10b48a89d104c899f32664cb32a70
Nick <nick.esselman@gmail.com>
2025-01-21 11:00:28 +0100

    added prefab and networktransforms

--- Assets/scripts/PlayerNetwork.cs
+++ Assets/scripts/PlayerNetwork.cs
@@ -1,40 +1,32 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerNetwork : NetworkBehaviour
+public class PlayerNetwork : MonoBehaviour
 {
-    public Transform head;
-    public Transform leftHand;
-    public Transform rightHand;
-
-    private NetworkVariable<Vector3> headPosition = new NetworkVariable<Vector3>();
-    private NetworkVariable<Quaternion> headRotation = new NetworkVariable<Quaternion>();
-    private NetworkVariable<Vector3> leftHandPosition = new NetworkVariable<Vector3>();
-    private NetworkVariable<Quaternion> leftHandRotation = new NetworkVariable<Quaternion>();
-    private NetworkVariable<Vector3> rightHandPosition = new NetworkVariable<Vector3>();
-    private NetworkVariable<Quaternion> rightHandRotation = new NetworkVariable<Quaternion>();
-
-    void Update()
+    public GameObject hostCamera;
+    
+    void Start()
     {
-        if (IsOwner)
+        if (Application.platform == RuntimePlatform.Android)
         {
-            // Update network variables with local VR transform data
-            headPosition.Value = head.position;
-            headRotation.Value = head.rotation;
-            leftHandPosition.Value = leftHand.position;
-            leftHandRotation.Value = leftHand.rotation;
-            rightHandPosition.Value = rightHand.position;
-            rightHandRotation.Value = rightHand.rotation;
+            StartClient();
         }
         else
         {
-            // Update local dummy transforms with synced data
-            head.position = headPosition.Value;
-            head.rotation = headRotation.Value;
-            leftHand.position = leftHandPosition.Value;
-            leftHand.rotation = leftHandRotation.Value;
-            rightHand.position = rightHandPosition.Value;
-            rightHand.rotation = rightHandRotation.Value;
+            StartHost();
         }
     }
+
+    void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+        hostCamera.SetActive(true);
+    }
+
+    void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+        hostCamera.SetActive(false);
+    }
+
 }
